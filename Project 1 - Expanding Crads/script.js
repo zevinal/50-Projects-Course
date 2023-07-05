08b642e52c2887d45f392945bd1c7607a47dd262
zevinal <ariankrasniqi23@gmail.com>
2023-07-05 06:27:38 +0100

    Fixed some formatting

--- Project 1 - Expanding Crads/script.js
+++ Project 1 - Expanding Crads/script.js
@@ -3,7 +3,7 @@ const panels = document.querySelectorAll('.panel')
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         removeActive()
-        panel.classList.add ('active')
+        panel.classList.add('active')
     })
 })
 function removeActive() {
